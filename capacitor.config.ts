1c143953311248b188f784f11670c82af0274d63
DariaSavu <143889190+DariaSavu@users.noreply.github.com>
2024-10-11 18:08:41 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.hikemanger',
+  appName: 'hikemanger',
+  webDir: 'dist'
+};
+
+export default config;
